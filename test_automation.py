ffc5e9359a82edf3d00231a0f8b881bed947ae53
ohmyrod <rldejournett01@gmail.com>
2025-09-30 13:19:22 -0400

    test: added automated test for empty content

--- test_automation.py
+++ test_automation.py
@@ -38,9 +38,34 @@ def test_add_valid_note():
     except Exception as e:
         print(f" -> FAILED: {e}")
 
+def test_add_note_empty_content():
+    """Test Case 2: Adding a note with empty content"""
+    print("Running Test: Adding a note with empty content")
+
+    try:
+        driver.refresh()
+        title_field = driver.find_element(By.NAME, "title")
+        # content_field = driver.find_element(By.NAME, "content")
+        submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
+
+        title_field.send_keys("Automated Test Note w/ empty Content")
+        # content_field.send_keys("This note was added by Selenium")
+        submit_button.click()
+
+        #Wait for the success message
+        WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "error"))
+        )
+        error_message = driver.find_element(By.CLASS_NAME, "error").text
+        assert "empty" in error_message.lower()
+        print(" -> PASSED (Error message shown)")
+    except Exception as e:
+        print(f" -> FAILED: {e}")
+
 if __name__ == "__main__":
 
     test_add_valid_note()
+    test_add_note_empty_content()
 
     #See result then quit
     time.sleep(3)
